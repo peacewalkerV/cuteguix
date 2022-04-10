f20964bccbbcfd1c3d46c4c19717a04c6054eb0c
John Doe <25673517+peacewalkerV@users.noreply.github.com>
2022-04-11 07:43:35 +0800

    Add modified keyboard layout.

--- installer.scm
+++ installer.scm
@@ -24,6 +24,11 @@
    (kernel linux)
    (firmware (list linux-firmware))
    
+   (keyboard-layout
+    (keyboard-layout
+     "us,ru"
+     #:options '("ctrl:nocaps" "compose:menu" "grp:alt_shift_toggle" "parens:swap_brackets")))
+   
    ;; Add the 'net.ifnames' argument to prevent network interfaces
    ;; from having really long names.  This can cause an issue with
    ;; wpa_supplicant when you try to connect to a wifi network.
